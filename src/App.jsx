68a27d22d6d7ca8ddfe637ffa970ffeab7d20019
ktomo <dreamguidance1@gmail.com>
2023-10-09 01:14:44 +0000

    add .jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>HELLO</h1>
+      <p>元気ですか</p>
+    </>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
-
-const App = () => {
-  return (
-    <>
-      <h1>HELLO</h1>
-      <p>元気ですか</p>
-    </>
-  );
-};
+import App from "./App";
 
 // HTMLのroot id の要素に APP のDOM内容を反映する
 ReactDom.render(<App />, document.getElementById("root"));
