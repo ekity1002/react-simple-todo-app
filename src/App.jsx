ecd37748ecece8ea4c136881cc0c8a0b3ca87540
ktomo <dreamguidance1@gmail.com>
2023-10-09 01:26:49 +0000

    style

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
+const onClick = () => alert("hoge");
+const contentStyle = {
+  color: "blue",
+  fontSize: "18px",
+};
 const App = () => {
   return (
     <>
-      <h1>HELLO</h1>
-      <p>元気ですか</p>
+      <h1 style={{ color: "red" }}>HELLO</h1>
+      <p style={contentStyle}>元気ですか</p>
+      <button onClick={onClick}>button</button>
     </>
   );
 };
