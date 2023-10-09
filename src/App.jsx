6835aff7927dc8033a9ea66aed0547a99923a30a
ktomo <dreamguidance1@gmail.com>
2023-10-09 04:58:28 +0000

    init for todo-app

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,7 @@ import React, { useState, useEffect } from "react";
 import { ColorfulMessage } from "./components/ColorfulMessage";
 
 const App = () => {
-  console.log("@@@@@@@@@@@@@@@@@@@@");
-  const onClickCountUp = () => {
-    setNum(num + 1);
-  };
-
-  const onClickSwitchFaceShowFlag = () => {
-    setFaceShowFlag(!faceShowFlag);
-  };
-
-  // setstateの引数にはState変数の初期値を指定できる
-  const [num, setNum] = useState(0); //Stateの変数名、Stateを変更する関数
-  const [faceShowFlag, setFaceShowFlag] = useState(false);
-  useEffect(() => {
-    console.log("num useEffect!!!!!");
-  }, [num]);
-
-  return (
-    <>
-      <h1 style={{ color: "red" }}>HELLO</h1>
-      <ColorfulMessage color="blue" message="元気ですか" />
-      <ColorfulMessage color="green" message="はげ" />
-      <ColorfulMessage color="red">はげてねえよ</ColorfulMessage>
-      <button onClick={onClickCountUp}>button</button>
-      <p>{num}</p>
-      <button onClick={onClickSwitchFaceShowFlag}>on/off</button>
-      {faceShowFlag && <p>(*^^*)</p>}
-    </>
-  );
+  return <div>test</div>;
 };
 
 export default App;
