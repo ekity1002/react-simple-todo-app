d7f7463536c7e2132bfe73beab683d92a95b1435
ktomo <dreamguidance1@gmail.com>
2023-10-09 02:53:18 +0000

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
-const onClick = () => alert("hoge");
-
 const App = () => {
+  console.log("@@@@@@@@@@@@@@@@@@@@");
   const onClickCountUp = () => {
     setNum(num + 1);
   };
 
+  const onClickSwitchFaceShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
   // setstateの引数にはState変数の初期値を指定できる
   const [num, setNum] = useState(0); //Stateの変数名、Stateを変更する関数
-
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+  useEffect(() => {
+    console.log("num useEffect!!!!!");
+  }, [num]);
+  
   return (
     <>
       <h1 style={{ color: "red" }}>HELLO</h1>
@@ -19,6 +26,8 @@ const App = () => {
       <ColorfulMessage color="red">はげてねえよ</ColorfulMessage>
       <button onClick={onClickCountUp}>button</button>
       <p>{num}</p>
+      <button onClick={onClickSwitchFaceShowFlag}>on/off</button>
+      {faceShowFlag && <p>(*^^*)</p>}
     </>
   );
 };
--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 
 const ColorfulMessage = (props) => {
-  console.log(props);
-  console.log({ props });
+  console.log("CloufulMessage");
   const contentStyle = {
     color: props.color,
     fontSize: "18px",
